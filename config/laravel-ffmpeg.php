1b5ebdd99b4c1b56ad4244d5df0a049ea03c5e3b
tawanda nyakudjga <tawanda.nyakudjga@gmail.com>
2019-09-05 00:28:42 +0800

    add docker file

--- config/laravel-ffmpeg.php
+++ config/laravel-ffmpeg.php
@@ -4,12 +4,12 @@ return [
     'default_disk' => 'local',
 
     'ffmpeg' => [
-        'binaries' => env('FFMPEG_BINARIES', 'ffmpeg'),
+        'binaries' => env('FFMPEG_BINARIES', '/usr/bin/ffmpeg'),
         'threads' => 12,
     ],
 
     'ffprobe' => [
-        'binaries' => env('FFPROBE_BINARIES', 'ffprobe'),
+        'binaries' => env('FFPROBE_BINARIES', '/usr/bin/ffprobe'),
     ],
 
     'timeout' => 3600,
